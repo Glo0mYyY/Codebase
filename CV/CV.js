1f175d19383db3ec5cadbddbdf297dab0dc845a1
Glo0mYyY <jan.niederhauser@hftm.ch>
2024-08-24 10:41:18 +0200

    optimized JS

--- CV/CV.js
+++ CV/CV.js
@@ -4,29 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
     { skill: "CSS", level: 5 },
     { skill: "Java Script", level: 1 },
     { skill: "PowerShell", level: 100 },
-
   ];
 
   const skillsContainer = document.getElementById("skills-container");
 
   skills.forEach(function (skill) {
     const skillDiv = document.createElement("div");
-    skillDiv.className = "skill";
-
-    const skillLabel = document.createElement("span");
-    skillLabel.innerText = skill.skill;
-    skillLabel.className = "skill-label";
-    skillDiv.appendChild(skillLabel);
-
-    const skillBar = document.createElement("div");
-    skillBar.className = "skill-bar";
-
-    const skillLevel = document.createElement("div");
-    skillLevel.className = "skill-level";
-    skillLevel.style.width = skill.level + "%";
-
-    skillBar.appendChild(skillLevel);
-    skillDiv.appendChild(skillBar);
+    skillDiv.innerHTML = `
+      <span class="skill-label">${skill.skill}</span>
+      <div class="skill-bar">
+        <div class="skill-level" style="width: ${skill.level}%"></div>
+      </div>
+    `;
 
     skillsContainer.appendChild(skillDiv);
   });
